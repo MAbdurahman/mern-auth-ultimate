fadf6b4e14e48c2f7f7d5f0f91af16ad51328651
MAbdurahman <mdbdrrhm@gmail.com>
2024-01-13 08:28:26 -0600

    Add components/auth/SignUp.jsx and update styles for components/auth/SignIn.jsx

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,12 +1,13 @@
 import Navbar from '../components/navbar/Navbar';
 import SignIn from '../components/auth/SignIn';
+import {SignUp} from '../components/auth/SignUp';
 
 export default function App() {
 
     return (
        <>
        <Navbar />
-          <SignIn />
+          <SignUp />
        </>
 
     );
--- frontend/src/components/auth/SignUp.jsx
+++ frontend/src/components/auth/SignUp.jsx
@@ -1,20 +1,19 @@
-
 import Container from '../Container';
 import Title from '../Title';
 import FormInput from '../forms/FormInput';
 import SubmitButton from '../forms/SubmitButton';
 
-
-export default function SignIn() {
+export function SignUp() {
    return (
-      <div className="fixed inset-0 bg-primary -z-10 flex justify-center items-center">
+      <div
+         className="fixed inset-0 bg-primary -z-10 flex justify-center items-center">
          <Container>
             <form className="bg-secondary rounded p-6 min-w-30 space-y-6 pb-9">
-               <Title>Sign In</Title>
-               <FormInput label="Email" placeholder="example@email.com"
-                          name="email"/>
+               <Title>Sign Up</Title>
+               <FormInput label="Name" placeholder="Full name" name="name" />
+               <FormInput label="Email" placeholder="example@email.com" name="email"/>
                <FormInput label="Password" placeholder="*************" name="password"/>
-               <SubmitButton value="Sign In"/>
+               <SubmitButton value="Sign Up"/>
                <div className="flex justify-end">
                   <a className="text-dark-subtle hover:text-white transition capitalize cursor-pointer"
                      href="/api/v1.0/auth/forgot-password"
@@ -22,9 +21,9 @@ export default function SignIn() {
                      Forget password&nbsp;/
                   </a>
                   <a className="text-dark-subtle hover:text-white transition capitalize cursor-pointer"
-                     href="/api/v1.0/auth/sign-up"
+                     href="/api/v1.0/auth/sign-in"
                   >
-                     &nbsp;Sign up
+                     &nbsp;Sign in
                   </a>
                </div>
             </form>
