6e11c4614e9f79502f68d31fb2418798d6aa4241
MAbdurahman <mdbdrrhm@gmail.com>
2024-01-13 13:45:46 -0600

    Complete components/auth/VerifyEmail.jsx

--- frontend/src/components/auth/VerifyEmail.jsx
+++ frontend/src/components/auth/VerifyEmail.jsx
@@ -1,13 +1,52 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import Container from '../Container';
 import Title from '../Title';
-import FormInput from '../forms/FormInput';
 import SubmitButton from '../forms/SubmitButton';
-import CustomLink from '../CustomLink';
+
 
 const OTP_LENGTH = 6;
+let currentOTPIndex;
 export default function VerifyEmail() {
    const [OTP, setOTP] = useState(new Array(OTP_LENGTH).fill(""));
+   const [activeOTPIndex, setActiveOTPIndex] = useState(0);
+
+   const inputRef = useRef();
+
+   const focusNextInputField = (index) => {
+      setActiveOTPIndex(index + 1);
+   };
+
+   const focusPrevInputField = (index) => {
+      let nextIndex;
+      const diff = index - 1;
+      nextIndex = diff !== 0 ? diff : 0;
+      setActiveOTPIndex(nextIndex);
+   };
+
+   const handleOTPChange = ({ target }) => {
+      const { value } = target;
+      const newOTP = [...OTP];
+      newOTP[currentOTPIndex] = value.substring(value.length - 1, value.length);
+
+      if (!value) {
+         focusPrevInputField(currentOTPIndex);
+      }
+      else {
+         focusNextInputField(currentOTPIndex);
+      }
+      setOTP([...newOTP]);
+   }
+   const handleKeyDown = ({ key }, index) => {
+      currentOTPIndex = index;
+      if (key === "Backspace") {
+         focusPrevInputField(currentOTPIndex);
+      }
+   };
+
+   useEffect(() => {
+      inputRef.current?.focus();
+   }, [activeOTPIndex]);
+
    return (
       <div className="fixed inset-0 bg-primary -z-10 flex justify-center items-center">
          <Container>
@@ -24,6 +63,10 @@ export default function VerifyEmail() {
                         <input
                            key={index}
                            type="number"
+                           ref={activeOTPIndex === index ? inputRef : null}
+                           value={OTP[index] || ""}
+                           onChange={handleOTPChange}
+                           onKeyDown={(e) => handleKeyDown(e, index)}
                            className="w-12 h-12 border-2 border-dark-subtle focus:border-white rounded bg-transparent outline-none text-center text-white font-semibold text-2xl"
                         />
                      );
