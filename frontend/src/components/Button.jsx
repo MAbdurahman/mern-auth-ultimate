b2962b09f7bf5463ec381a27ff4bb8363c967b25
MAbdurahman <mdbdrrhm@gmail.com>
2024-01-13 08:47:29 -0600

    Add frontend/src/pages/HomePage.jsx and frontend/src/components/Button.jsx

---
+++ frontend/src/components/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {useNavigate} from 'react-router-dom';
+
+export default function Button({children}) {
+   const navigate = useNavigate();
+
+   function handleClick() {
+      navigate('/api/v1.0/auth/sign-in');
+   }
+
+   return (<button onClick={handleClick}
+                   className="bg-gray-200 hover:bg-gray-300 py-4 px-8 rounded-md border-none uppercase font-bold text-lg text-logo-blue cursor-pointer">
+      {children}
+   </button>)
+}
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import Button from '../components/Button.jsx';
+
+export default function HomePage() {
+
+   return (<div
+      className="flex flex-col justify-center items-center bg-gray-800 w-full h-screen">
+      <h1 className="mb-5 text-5xl text-white font-extrabold">MERN Auth Ultimate</h1>
+      <h2
+         className="mb-7 px-2 text-2xl text-center text-gray-300 font-medium">Keep
+         your profile information in one secure
+         location,<br/> accessible to you everywhere</h2>
+      <Button>Start Now</Button>
+   </div>)
+}
