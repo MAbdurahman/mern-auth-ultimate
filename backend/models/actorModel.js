2a1de3bc0f17a867081e2dd77d7d2af585295181
MAbdurahman <mdbdrrhm@gmail.com>
2024-02-12 03:32:48 -0600

    Add actorModel.js to backend/models/actorModel.js

---
+++ backend/models/actorModel.js
@@ -0,0 +1,29 @@
+//**************** imports ****************//
+import mongoose from 'mongoose';
+
+const actorModel = new mongoose.Schema({
+      name: {
+         type: String,
+         trim: true,
+         required: true,
+      },
+      about: {
+         type: String,
+         trim: true,
+         required: true,
+      },
+      gender: {
+         type: String,
+         trim: true,
+         required: true,
+      },
+      avatar: {
+         type: Object,
+         url: String,
+         public_id: String,
+      }
+   },
+   {timestamps: true});
+const Actor = mongoose.model('Actor', actorModel);
+
+export default Actor;
