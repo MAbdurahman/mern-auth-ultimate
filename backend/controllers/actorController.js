c7fbc09508c010ff47c673beba0adc4f0eb0b46a
MAbdurahman <mdbdrrhm@gmail.com>
2024-02-12 10:13:11 -0600

    Add cloudinary package and update creatActor in controllers/actorControlller.js

--- backend/controllers/actorController.js
+++ backend/controllers/actorController.js
@@ -1,9 +1,38 @@
-
+import cloudinary from 'cloudinary';
 import Actor from '../models/actorModel.js';
+import {errorMessageHandler} from '../utils/errorMessageUtils.js';
 
+cloudinary.v2.config({
+   cloud_name: process.env.CLOUDINARY_NAME,
+   api_key: process.env.CLOUDINARY_API_KEY,
+   api_secret: process.env.CLOUDINARY_API_SECRET,
+   secure: true
+});
 
 
 export const createActor = async (req, res, next) => {
+   const {name, biography, gender} = req.body;
+   const {file} = req;
 
-   await console.log(req.body);
+   try {
+      const newActor = new Actor({name, biography, gender});
+
+      if (file) {
+         /*const { secure_url, public_id } = await cloudinary.v2.uploader.upload(file.path);*/
+         const {secure_url, publci_id} = await cloudinary.v2.uploader.upload(file, {
+            folder: 'mern-auth-ultimate/actors', width: 150, crop: 'scale'
+         });
+         newActor.avatar = {url: secure_url, public_id};
+
+      }
+      await newActor.save();
+      res.status(201).json({
+         id: newActor._id, name, biography, gender, avatar: newActor.avatar?.url,
+      });
+   }
+   catch (err) {
+      return errorMessageHandler(res, err, 501);
+      next();
+   }
+
 };
